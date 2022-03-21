bd5cdd773ee5559d634ae560d7e6a1bb20fb423d
edison <edison@ucrop.it>
2022-03-20 21:30:17 -0400

    ES7 example

---
+++ src/ES7/index.js
@@ -0,0 +1,15 @@
+let numbers = [1, 2, 6, 7, 8];
+
+//ES7 INCLUDES
+if (numbers.includes(7)) {
+  console.log("Si se encuentra el valor 7");
+} else {
+  console.log("No se encuentra");
+}
+
+let base = 4;
+let exponent = 3;
+
+//ES7 EXPONENT
+let result = base ** exponent;
+console.log(result);
